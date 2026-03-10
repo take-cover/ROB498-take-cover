ae5896c2061f7e3de31f1285c941796c1889eb74
Linda Zhao <linda.zhao23@gmail.com>
2026-03-10 19:01:02 -0400

    use transforms in vision node; make var naming consistent

--- flight-exercise-3/comm_node.py
+++ flight-exercise-3/comm_node.py
@@ -17,12 +17,9 @@ HOVER_Z = 0.5 # [m]
 FREQ_30_HZ = 1/30 # [1/Hz]
 FREQ_0_5_HZ = 2 # [1/Hz]
 FREQ_10_HZ = 1/10 # [1/Hz]
-LANDING_TOL = 0.1 # [m]
-LANDED_TOL = 0.005 # [m]
-VICON_DRONE_GND_Z = 0.18 # [m] UPDATE WITH REAL VICON MEASUREMENT
 
 LOG_LATEST_POSE = True
-LOG_WAYPOINT = False
+LOG_SETPOINT = False
 
 WAYPOINTS = None
 WAYPOINTS_RECEIVED = False
@@ -34,16 +31,21 @@ WAYPOINT_HOLD_TIME = 0.5   # [s]
 class CommNode(Node):
     def __init__(self):
         super().__init__('rob498_drone_1')
-        # Poses
-        self.initial_pose = None # Startup pose (first power on)
-        self.latest_pose = None
-        self.setpoint_pose = PoseStamped() # Current setpoint
-
-        self.use_vicon = False
-        self.vicon_initial_pose = None
-        # self.vicon_to_camera_tf = None
+        # Init pose variables
+        self.initial_pose = None # Startup pose in Vicon frame
+        self.latest_pose = None # Always in Vicon frame
         
+        # Init camera to Vicon transform
+        self.cam_to_vicon_tf = None
+        
+        self.setpoint_pose = PoseStamped() # Current setpoint
         self.state = State()
+        
+        # Set up waypoint FSM
+        self.create_timer(FREQ_10_HZ, self.run_waypoint_fsm)
+        self.fsm_active = False
+        self.fsm_waypoint_index = 0
+        self.fsm_hold_start_time = None
 
         # Set up publishers
         self.setpoint_pub = self.create_publisher(
@@ -54,18 +56,6 @@ class CommNode(Node):
         self.create_timer(FREQ_30_HZ, self.publish_setpoint) # publish waypoint at 30Hz
         self.create_timer(FREQ_0_5_HZ, self.print_setpoint)
         
-
-        # waypoint setup
-        # waypoint FSM
-        self.create_timer(FREQ_10_HZ, self.run_waypoint_fsm)
-        self.fsm_active = False
-        self.fsm_waypoint_index = 0
-        self.fsm_hold_start_time = None
-
-        # subtract offset if using camera
-        self.create_timer(FREQ_0_5_HZ, self.subtract_offset_to_waypoints)
-        self.subtract_waypoint_offset_done = False
-        
         # Set up subscribers
         self.ego_sub = self.create_subscription(
             PoseStamped,
@@ -73,30 +63,30 @@ class CommNode(Node):
             self.mavros_vision_pose_callback,
             qos_profile_system_default
         )
-        self.ego_init_sub = self.create_subscription(
+        self.init_cam_pose_sub = self.create_subscription(
             PoseStamped,
-            "/team1_fe3/vision_pose/initial_pose",
-            self.mavros_vision_initial_pose_callback,
+            "/team1_fe3/vision_pose/initial_cam_pose",
+            self.initial_cam_pose_callback,
             qos_profile_system_default
         )
-        self.vicon_ego_init_sub = self.create_subscription(
+        self.init_vicon_pose_sub = self.create_subscription(
             PoseStamped,
-            "team1_fe3/vision_pose/vicon_initial_pose",
-            self.vicon_initial_pose_callback,
+            "/team1_fe3/vision_pose/initial_vicon_pose",
+            self.initial_vicon_pose_callback,
             qos_profile_system_default
         )
-        self.sub_waypoints = self.create_subscription(
+        self.waypoints_sub = self.create_subscription(
             PoseArray, 
-            'rob498_drone_1/comm/waypoints', 
-            self.callback_waypoints, 
+            '/rob498_drone_1/comm/waypoints', 
+            self.waypoints_callback, 
             qos_profile_system_default
         )
-        # self.vicon_to_camera_tf = self.create_subscription(
-        #     TransformStamped, 
-        #     '/team1_fe3/vicon_to_camera_transform', 
-        #     self.callback_vicon_to_camera_tf, 
-        #     qos_profile_system_default
-        # )
+        self.cam_to_vicon_tf_sub = self.create_subscription(
+            TransformStamped, 
+            '/team1_fe3/cam_to_vicon_tf', 
+            self.cam_to_vicon_tf_callback, 
+            qos_profile_system_default
+        )
 
         # Set up mavros clients
         self.arming_client = self.create_client(CommandBool, "mavros/cmd/arming")
@@ -155,7 +145,7 @@ class CommNode(Node):
         target_pose.header.frame_id = "map"
         target_pose.pose.position.x = self.initial_pose.pose.position.x
         target_pose.pose.position.y = self.initial_pose.pose.position.y
-        target_pose.pose.position.z = self.initial_pose.pose.position.z + HOVER_Z - VICON_DRONE_GND_Z # set target to hover at desired height above initial position
+        target_pose.pose.position.z = HOVER_Z
 
         target_pose.pose.orientation.x = self.initial_pose.pose.orientation.x
         target_pose.pose.orientation.y = self.initial_pose.pose.orientation.y
@@ -191,11 +181,6 @@ class CommNode(Node):
         if self.initial_pose is None:
             response.success = False
             response.message = "No initial pose."
-            return response
-        
-        if not self.use_vicon and not self.subtract_waypoint_offset_done:
-            response.success = False
-            response.message = "Vicon offset not subtracted yet."
             return response
 
         if self.state.mode != "OFFBOARD":
@@ -244,36 +229,42 @@ class CommNode(Node):
         current_pose.header.stamp = self.get_clock().now().to_msg()
         current_pose.header.frame_id = "map"
         current_pose.pose = msg.pose
-
-        self.latest_pose = current_pose
-
-
-    def mavros_vision_initial_pose_callback(self, msg):
-        current_pose = PoseStamped()
-        current_pose.header.stamp = self.get_clock().now().to_msg()
-        current_pose.header.frame_id = "map"
-        current_pose.pose = msg.pose
-
+        
         if self.initial_pose is None:
             self.initial_pose = current_pose
             self.get_logger().info(f"Set initial pose: x={self.initial_pose.pose.position.x}, y={self.initial_pose.pose.position.y}, z={self.initial_pose.pose.position.z}")
-        
 
-    def vicon_initial_pose_callback(self, msg):
+        self.latest_pose = current_pose
+        if LOG_LATEST_POSE:
+            self.get_logger().info(f"Latest pose: x={self.latest_pose.pose.position.x}, y={self.latest_pose.pose.position.y}, z={self.latest_pose.pose.position.z}")
+
+
+    def initial_cam_pose_callback(self, msg):
         current_pose = PoseStamped()
         current_pose.header.stamp = self.get_clock().now().to_msg()
         current_pose.header.frame_id = "map"
         current_pose.pose = msg.pose
 
-        if self.vicon_initial_pose is None:
-            self.vicon_initial_pose = current_pose
-            self.get_logger().info(f"Set Vicon initial pose: x={self.vicon_initial_pose.pose.position.x}, y={self.vicon_initial_pose.pose.position.y}, z={self.vicon_initial_pose.pose.position.z}")
+        if self.initial_cam_pose is None:
+            self.initial_cam_pose = current_pose
+            self.get_logger().info(f"Set initial camera pose: x={self.initial_cam_pose.pose.position.x}, y={self.initial_cam_pose.pose.position.y}, z={self.initial_cam_pose.pose.position.z}")
+        
+
+    def initial_vicon_pose_callback(self, msg):
+        current_pose = PoseStamped()
+        current_pose.header.stamp = self.get_clock().now().to_msg()
+        current_pose.header.frame_id = "map"
+        current_pose.pose = msg.pose
+
+        if self.initial_vicon_pose is None:
+            self.initial_vicon_pose = current_pose
+            self.get_logger().info(f"Set initial Vicon pose: x={self.initial_vicon_pose.pose.position.x}, y={self.initial_vicon_pose.pose.position.y}, z={self.initial_vicon_pose.pose.position.z}")
         
 
     ############################################################################
-    # Waypoints callback (TODO)
+    # Waypoints callback
     ############################################################################
-    def callback_waypoints(self, msg):
+    def waypoints_callback(self, msg):
         global WAYPOINTS_RECEIVED, WAYPOINTS
         if WAYPOINTS_RECEIVED:
             return
@@ -284,27 +275,6 @@ class CommNode(Node):
             pos = np.array([[pose.position.x], [pose.position.y], [pose.position.z]])
             WAYPOINTS = np.hstack((WAYPOINTS, pos))
 
-    def subtract_offset_to_waypoints(self):
-        global WAYPOINTS_RECEIVED, WAYPOINTS
-        if self.use_vicon or \
-            not WAYPOINTS_RECEIVED or \
-            WAYPOINTS is None or \
-            self.vicon_initial_pose is None or \
-            self.subtract_waypoint_offset_done:
-            return
-        
-        # subtract vicon_initial_pose from waypoints
-        vicon_x = self.vicon_initial_pose.pose.position.x
-        vicon_y = self.vicon_initial_pose.pose.position.y
-        vicon_z = self.vicon_initial_pose.pose.position.z
-
-        for i in range(WAYPOINTS.shape[1]):
-            WAYPOINTS[0,i] -= vicon_x
-            WAYPOINTS[1,i] -= vicon_y
-            WAYPOINTS[2,i] -= vicon_z
-        
-        self.subtract_waypoint_offset_done = True
-        self.get_logger().info("Vicon offset subtracted from waypoints.")
 
     def update_waypoint_target(self, waypoint_index):
         """Set self.setpoint_pose to waypoint at index from WAYPOINTS (shape 3xN)."""
@@ -320,10 +290,6 @@ class CommNode(Node):
 
         if self.latest_pose is not None:
             new_waypoint.pose.orientation = self.latest_pose.pose.orientation
-
-        # Transform waypoint from vicon frame to camera frame
-        # if self.vicon_to_camera_tf is not None:
-        #     new_waypoint = transform_pose(new_waypoint, self.vicon_to_camera_tf)
 
         self.setpoint_pose = new_waypoint
 
@@ -370,28 +336,6 @@ class CommNode(Node):
         else:
             self.fsm_hold_start_time = None
             
-    ############################################################################
-    # Vicon to Camera Transform Callback
-    ############################################################################
-    def callback_vicon_to_camera_tf(self, msg):
-        """Update waypoint target by transforming from vicon frame to camera frame."""
-        if msg is None:
-            self.get_logger().info("Received empty transform message.")
-            return
-        
-        # Convert PoseStamped to TransformStamped for transformation function
-        transform = TransformStamped()
-        transform.header = msg.header
-        transform.child_frame_id = msg.child_frame_id
-        transform.transform.translation.x = msg.transform.translation.x
-        transform.transform.translation.y = msg.transform.translation.y
-        transform.transform.translation.z = msg.transform.translation.z
-        transform.transform.rotation.x = msg.transform.rotation.x
-        transform.transform.rotation.y = msg.transform.rotation.y
-        transform.transform.rotation.z = msg.transform.rotation.z
-        transform.transform.rotation.w = msg.transform.rotation.w
-
-        self.vicon_to_camera_tf = transform
 
     ############################################################################
     # Publisher functions
@@ -399,7 +343,7 @@ class CommNode(Node):
     def publish_setpoint(self):
         self.setpoint_pose.header.stamp = self.get_clock().now().to_msg()
         self.setpoint_pub.publish(self.setpoint_pose)
-        if LOG_WAYPOINT:
+        if LOG_SETPOINT:
             self.get_logger().info(f"Published setpoint: x={self.setpoint_pose.pose.position.x}, y={self.setpoint_pose.pose.position.y}, z={self.setpoint_pose.pose.position.z}")
 
 
@@ -415,6 +359,7 @@ def main(args=None):
     node = CommNode()
     rclpy.spin(node)
     rclpy.shutdown()
+
 
 if __name__ == "__main__":
     main()
