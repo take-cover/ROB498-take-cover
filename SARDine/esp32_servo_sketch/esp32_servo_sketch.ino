f389e01f1c7fdd44eb0509da774a8771c4a9a0a6
richard-hanxu <richardhanxu@gmail.com>
2026-03-27 13:49:18 -0400

    added mcu code

---
+++ SARDine/esp32_servo_sketch/esp32_servo_sketch.ino
@@ -0,0 +1,42 @@
+#include <ESP32Servo.h>
+
+Servo myservo;
+const int servoSwitchPin = 2;   
+const int sensorSwitchPin = 4;
+const int servoPin = 19; // Output for servo control
+const int ledPin = 21;    // Output to LED
+
+void setup() {
+  Serial.begin(115200);
+
+  // Configure Inputs
+  pinMode(servoSwitchPin, INPUT);
+  pinMode(sensorSwitchPin, INPUT);
+  
+  // Configure Output
+  pinMode(ledPin, OUTPUT);
+
+  // Servo Setup
+  ESP32PWM::allocateTimer(0);
+  myservo.setPeriodHertz(50);
+  myservo.attach(servoPin, 500, 2400); 
+}
+
+void loop() {
+  // 1. Continuous Servo Logic
+  if (digitalRead(servoSwitchPin) == HIGH) {
+    myservo.write(180);
+  } else {
+    myservo.write(0);
+  }
+
+  // 2. LED Logic 
+  if (digitalRead(ledSwitchPin) == HIGH) {
+    digitalWrite(ledPin, HIGH); // Turn LED ON
+  } else {
+    digitalWrite(ledPin, LOW);  // Turn LED OFF
+  }
+
+  // Small delay for stability
+  delay(20); 
+}
